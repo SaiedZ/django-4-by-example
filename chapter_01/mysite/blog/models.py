b895ba7d30a85d360da963c05021b97614bce82c
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-03-06 08:40:51 +0100

    Add author field to Post model

--- chapter_01/mysite/blog/models.py
+++ chapter_01/mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
@@ -8,6 +9,11 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
