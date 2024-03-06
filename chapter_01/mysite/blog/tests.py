15b9d301eacd1cd58a233948098b84d10f9bb7ac
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-03-06 19:10:32 +0100

    Add tests for post list and detail views

--- chapter_01/mysite/blog/tests.py
+++ chapter_01/mysite/blog/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Post
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_post_list_view(client):
+    url = reverse('blog:post_list')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_post_detail_view(client):
+    user = User.objects.create_user(username='user', password='testpass')
+    post = Post.objects.create(
+        author=user,
+        title='Test Post',
+        slug='test-post',
+        body='Test Content',
+        status=Post.Status.PUBLISHED,
+    )
+    url = reverse('blog:post_detail', args=[post.id])
+    response = client.get(url)
+    assert response.status_code == 200
+    assert 'Test Post' in response.content.decode()
