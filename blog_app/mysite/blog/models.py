06eb8013e60334ea7a9c2c8f30028469846d58ff
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-03-22 15:12:50 +0100

    Add __repr__ method to Post model

--- blog_app/mysite/blog/models.py
+++ blog_app/mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -41,3 +42,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def __repr__(self) -> str:
+        return f"Post(title={self.title}, author={self.author}), id={self.id})"
