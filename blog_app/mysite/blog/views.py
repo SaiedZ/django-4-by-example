d3242a4af88b70048b8dcc57183b197ee20fc681
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-03-22 15:42:14 +0100

    Refactor post_list view and add alternative PostListView class

--- blog_app/mysite/blog/views.py
+++ blog_app/mysite/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from django.core.paginator import (
     Paginator, EmptyPage, PageNotAnInteger
 )
@@ -6,20 +7,30 @@ from django.core.paginator import (
 from .models import Post
 
 
-def post_list(request):
+# def post_list(request):
 
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 1)
-    page_number = request.GET.get('page', 1)
+#     post_list = Post.published.all()
+#     paginator = Paginator(post_list, 1)
+#     page_number = request.GET.get('page', 1)
 
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
 
-    return render(request, "blog/post/list.html", {"posts": posts})
+#     return render(request, "blog/post/list.html", {"posts": posts})
+
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 1
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year: int, month: int, day: int, post: str):
