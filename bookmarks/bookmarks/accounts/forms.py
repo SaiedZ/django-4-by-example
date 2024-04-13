18b631a85b88fb6b3488a1fe19775c5a7411b6d9
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-04-13 22:57:33 +0200

    feat: Add UserEditForm and ProfileEditForm to accounts app

--- bookmarks/bookmarks/accounts/forms.py
+++ bookmarks/bookmarks/accounts/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth import get_user_model
 
+from .models import Profile
+
 User = get_user_model()
 
 
@@ -30,3 +32,17 @@ class UserRegistrationForm(forms.ModelForm):
         if cleaned_data['password'] != cleaned_data['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cleaned_data['password2']
+
+
+class UserEditForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+
+class ProfileEditForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth', 'photo']
