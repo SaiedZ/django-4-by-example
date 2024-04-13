53d072f7234adc07ddf620b85deabbda6599d2a2
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-04-13 22:57:45 +0200

    feat: Update admin interface to include Profile model inline

--- bookmarks/bookmarks/accounts/admin.py
+++ bookmarks/bookmarks/accounts/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import Profile
+
+User = get_user_model()
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'date_of_birth', 'photo']
     raw_id_fields = ['user']
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
