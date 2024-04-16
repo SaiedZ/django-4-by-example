196a1ba011b7a147ba3ac4ae52542d36d10592a6
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-04-16 22:07:48 +0200

    feat: Update ImageCreateForm to use httpx for image download

--- bookmarks/bookmarks/images/forms.py
+++ bookmarks/bookmarks/images/forms.py
@@ -1,4 +1,10 @@
+import requests
+import httpx
+
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 from django import forms
+
 from .models import Image
 
 
@@ -16,9 +22,47 @@ class ImageCreateForm(forms.ModelForm):
         valid_extensions = ['jpg', 'jpeg']
 
         url = self.cleaned_data['url']
-        extension = url.rsplit('.', 1)[1].lower()
+        extension = self._get_extension(url)
         if extension not in valid_extensions:
             raise forms.ValidationError(
                 'The given URL does not match valid image extensions.'
             )
         return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = self._get_extension(image_url)
+        image_name = f'{name}.{extension}'
+
+        with httpx.Client() as client:
+            response = client.get(image_url)
+            if response.status_code == 200:
+                image.image.save(image_name, ContentFile(
+                    response.content), save=False)
+            else:
+                raise forms.ValidationError(
+                    f"The given URL, {image_url}, does not match valid "
+                    f"image extensions. Status code: {response.status_code}"
+                )
+
+        if commit:
+            image.save()
+        return image
+
+    # def save(self, force_insert=False, force_update=False, commit=True):
+    #     image = super().save(commit=False)
+    #     image_url = self.cleaned_data['url']
+    #     name = slugify(image.title)
+    #     extension = self._get_extension(image_url)
+    #     image_name = f'{name}.{extension}'
+    #     # download image from the given URL
+    #     response = requests.get(image_url)
+    #     image.image.save(image_name, ContentFile(response.content), save=False)
+    #     if commit:
+    #         image.save()
+    #     return image
+
+    def _get_extension(self, url):
+        return url.rsplit('.', 1)[1].lower()
