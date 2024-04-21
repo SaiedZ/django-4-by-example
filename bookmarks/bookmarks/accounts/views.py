d40e3d273687fe075dfced718dd3a84de1b0e6ec
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-04-21 21:02:06 +0200

    feat: Update user dashboard to display recent actions
    The code changes in the `accounts/views.py` file update the `dashboard` function to display recent user actions on the dashboard page. The actions are retrieved from the `Action` model and filtered based on the user's following list. The updated functionality enhances the user experience by showing relevant and personalized activity on the dashboard.

--- bookmarks/bookmarks/accounts/views.py
+++ bookmarks/bookmarks/accounts/views.py
@@ -1,18 +1,39 @@
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 
+from actions.utils import create_action
+from actions.models import Action
+
+from .models import Contact, Profile
 from .forms import (
     LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 )
-from .models import Profile
+
+
+User = get_user_model()
 
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    actions = Action.objects.select_related(
+        'user__profile'
+    ).prefetch_related('target').exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions[:10]
+    return render(
+        request,
+        'account/dashboard.html',
+        {'section': 'dashboard', 'actions': actions}
+    )
 
 
 def register(request: HttpRequest) -> HttpResponse:
@@ -22,7 +43,8 @@ def register(request: HttpRequest) -> HttpResponse:
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
-            Profile.objects.create(user=new_user)
+            Profile.objects.get_or_create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(
                 request, 'account/register_done.html', {'new_user': new_user}
             )
@@ -53,6 +75,53 @@ def edit(request: HttpRequest) -> HttpResponse:
         'account/edit.html',
         {'user_form': user_form, 'profile_form': profile_form}
     )
+
+
+@login_required
+def user_list(request):
+    users = User.objects.select_related("profile").filter(
+        is_active=True).exclude(id=request.user.id)
+    return render(request,
+                  'user/list.html',
+                  {'section': 'people',
+                   'users': users})
+
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User,
+                             username=username,
+                             is_active=True)
+    return render(request,
+                  'user/detail.html',
+                  {'section': 'people',
+                   'user': user})
+
+
+@require_POST
+@login_required
+def user_follow(request) -> JsonResponse:
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+
+    if user_id and action:
+
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                # Contact.objects.get_or_create(user_from=request.user,
+                #                              user_to=user)
+                request.user.following.add(user)
+                create_action(request.user, 'is following', user)
+            else:
+                Contact.objects.filter(user_from=request.user,
+                                       user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+
+    return JsonResponse({'status': 'error'})
 
 
 # def user_login(request: HttpRequest) -> HttpResponse:
