52772096c6e1a98fa329b547d17df3ef4a42db4e
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-04-30 16:54:02 +0200

    feat: Update Redis configuration for caching and add new dependencies
    feat: Update Redis configuration for caching and add new dependencies

--- bookmarks/bookmarks/core/settings.py
+++ bookmarks/bookmarks/core/settings.py
@@ -212,6 +212,18 @@ ABSOLUTE_URL_OVERRIDES = {
                                         args=[u.username])
 }
 
+# CACHES
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 
 # Redis
 
