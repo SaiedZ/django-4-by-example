29dbff3a32ff4515545f5ffb38a63c9c00866328
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-04-30 15:54:09 +0200

    feat: Add signals for updating total_likes field in Image model
    The code changes in the `images` app introduce signals to update the `total_likes` field in the `Image` model. This ensures that the `total_likes` field accurately reflects the number of users who have liked the image. By using signals, the `total_likes` field is automatically updated whenever a user likes or unlikes an image, improving the consistency and reliability of the data.

--- bookmarks/bookmarks/images/apps.py
+++ bookmarks/bookmarks/images/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class ImagesConfig(AppConfig):
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'images'
+
+    def ready(self) -> None:
+        # register signals
+        import images.signals  # noqa
