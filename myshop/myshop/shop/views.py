5529b23bdcd67ffdb829b3b9cbd6510feeac8caf
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-05-06 22:01:31 +0200

    add cart handeling to add product

--- myshop/myshop/shop/views.py
+++ myshop/myshop/shop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
+from cart.forms import CartAddProductForm
+
 from .models import Category, Product
 
 
@@ -26,9 +28,9 @@ def product_detail(request, id, slug):
         Product.objects.select_related('category'),
         id=id, slug=slug, available=True
     )
-
+    cart_product_form = CartAddProductForm()
     return render(
         request,
         'shop/product/detail.html',
-        {'product': product}
+        {'product': product, "cart_product_form": cart_product_form},
     )
