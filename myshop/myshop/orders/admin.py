689b414ef30a9177f382eb14a4387d9ff6be3766
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-05-13 20:11:20 +0200

    chore: Update OrderAdmin list_display to include Stripe payment

--- myshop/myshop/orders/admin.py
+++ myshop/myshop/orders/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Order, OrderItem
+
+
+def order_payment(obj):
+    if not obj.stripe_id:
+        return ''
+
+    url = obj.get_stripe_url()
+    html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+    return mark_safe(html)
+
+
+order_payment.short_description = 'Stripe payment'
 
 
 class OrderItemInline(admin.TabularInline):
@@ -11,6 +24,6 @@ class OrderItemInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
+                    order_payment, 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
