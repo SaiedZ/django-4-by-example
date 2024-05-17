3074082df902050305271a3b7ceb2fccb817662c
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-05-17 18:24:46 +0200

    feat: Add discount functionality to Order model

--- myshop/myshop/orders/models.py
+++ myshop/myshop/orders/models.py
@@ -1,10 +1,25 @@
+from decimal import Decimal
+
 from django.db import models
 from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 
+from coupons.models import Coupon
 from shop.models import Product
 
 
 class Order(models.Model):
+    coupon = models.ForeignKey(
+        Coupon,
+        related_name='orders',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    discount = models.IntegerField(
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
 
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -26,8 +41,18 @@ class Order(models.Model):
     def __str__(self):
         return f'Order {self.id}'
 
-    def get_total_cost(self):
+    def get_total_cost_before_discount(self):
         return sum(item.get_cost() for item in self.items.all())
+
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return (self.discount / Decimal(100)) * total_cost
+        return Decimal(0)
+
+    def get_total_cost(self):
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
 
     def get_stripe_url(self):
         if not self.stripe_id:
