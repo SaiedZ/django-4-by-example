3c4bf8efd7c40c06c82038f059cb5d48a4966e6a
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-05-17 18:25:15 +0200

    fix coupon_apply view to use 'cart:detail' for redirect

--- myshop/myshop/coupons/views.py
+++ myshop/myshop/coupons/views.py
@@ -21,4 +21,4 @@ def coupon_apply(request):
             request.session['coupon_id'] = coupon.id
         except Coupon.DoesNotExist:
             request.session['coupon_id'] = None
-    return redirect('cart:cart_detail')
+    return redirect('cart:detail')
