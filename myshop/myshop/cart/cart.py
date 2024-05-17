883b40a1a697935ce0942d194ab940255eb2645a
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-05-17 17:48:53 +0200

    chore: Add coupon functionality to the shopping cart

--- myshop/myshop/cart/cart.py
+++ myshop/myshop/cart/cart.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 
 from django.conf import settings
 from shop.models import Product
+from coupons.models import Coupon
 
 
 class Cart:
@@ -9,6 +10,9 @@ class Cart:
     def __init__(self, request):
         self.session = request.session
         self.cart = self.session.setdefault(settings.CART_SESSION_ID, {})
+
+        # store current applied coupon
+        self.coupon_id = self.session.get('coupon_id')
 
     def add(self, product, quantity=1, update_quantity=False):
         product_id = str(product.id)
@@ -66,3 +70,20 @@ class Cart:
         # remove cart from session
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    @property
+    def coupon(self):
+        if not self.coupon_id:
+            return None
+        try:
+            return Coupon.objects.get(id=self.coupon_id)
+        except Coupon.DoesNotExist:
+            return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal(100)) * self.get_total_price()
+        return Decimal(0)
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
