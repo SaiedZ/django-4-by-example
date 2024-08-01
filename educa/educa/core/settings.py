d3f6abce9c1d17a0085c1e6d88da542d5f921259
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-08-01 11:27:02 +0200

    feat: Add LOGIN_REDIRECT_URL setting for student course list redirection

--- educa/educa/core/settings.py
+++ educa/educa/core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from django.urls import reverse_lazy
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,3 +131,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
