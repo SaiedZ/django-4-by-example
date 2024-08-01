68a62b9adb3541373ef1404993e653d32692ba5a
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-08-01 10:29:56 +0200

    feat: Add CourseListView for displaying and filtering courses
    This code change adds the CourseListView to the courses app. The CourseListView is responsible for displaying and filtering courses based on subjects. It retrieves the subjects and courses from the database, and allows users to filter courses by subject. This enhancement improves the functionality of the application by providing a dedicated view for listing and filtering courses.

--- educa/educa/courses/views.py
+++ educa/educa/courses/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMix
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.base import TemplateResponseMixin, View
+from django.db.models import Count
 
 from django.forms.models import modelform_factory
 from django.apps import apps
@@ -11,7 +12,7 @@ from django.apps import apps
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
 from .forms import ModuleFormSet
-from .models import Module, Content, Course
+from .models import Module, Content, Course, Subject
 
 
 class OwnerMixin:
@@ -245,4 +246,28 @@ class ContentOrderView(CsrfExemptMixin,
 
         return self.render_json_response(
             {'saved': 'OK'}
+        )
+
+
+class CourseListView(TemplateResponseMixin, View):
+
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(
+            total_courses=Count('courses')
+        )
+        courses = Course.objects.annotate(
+            total_modules=Count('modules')
+        )
+
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+
+        return self.render_to_response(
+            {'subjects': subjects,
+             'subject': subject,
+             'courses': courses}
         )
