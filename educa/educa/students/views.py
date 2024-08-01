c2461639e079b3b259e3a1801b135a9f5bd5ff03
SaiedZ <saied.zouaoui.pro@gmail.com>
2024-08-01 11:17:59 +0200

    feat: Add StudentEnrollCourseView for student course enrollment
    This code change adds the `StudentEnrollCourseView` to the `students` app. The `StudentEnrollCourseView` is responsible for handling the enrollment of students in courses. It includes a form for students to select a course and enrolls the student in the selected course upon form submission. This enhancement improves the functionality of the application by providing a dedicated view for student course enrollment.

--- educa/educa/students/views.py
+++ educa/educa/students/views.py
@@ -19,3 +19,17 @@ class StudentRegistrationView(CreateView):
         user = authenticate(username=cd['username'], password=cd['password1'])
         login(self.request, user)
         return result
+
+
+class StudentEnrollCourseView(LoginRequiredMixin, FormView):
+
+    course = None
+    form_class = CourseEnrollForm
+
+    def form_valid(self, form):
+        self.course = form.cleaned_data['course']
+        self.course.students.add(self.request.user)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('student_course_detail', args=[self.course.id])
